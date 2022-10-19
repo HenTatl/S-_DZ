ef39b686b8c7e8c22308d1acf1da31e512d0e56c
HenTatl <alien2amik@gmail.com>
2022-10-19 22:56:25 +0200

    zadacha4

---
+++ Sem1_DZ2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: 
+//Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел
+
+Console.WriteLine("Enter the First number :");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a Second number :");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a Third number :");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+
+else
+{
+    Console.WriteLine($" Maximum value is {max}");
+}
