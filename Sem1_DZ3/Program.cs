00ab5d89208dc2c44aa893c42c42906fa1b93fdb
HenTatl <alien2amik@gmail.com>
2022-10-19 23:16:47 +0200

    zadacha6

---
+++ Sem1_DZ3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 6: 
+//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter number :");
+
+int numberA = int.Parse(Console.ReadLine());
+
+if (numberA % 2 == 0)
+
+{
+    Console.WriteLine(numberA + " is an even number");
+}
+
+else
+
+{
+    Console.WriteLine(numberA + " is an odd number");
+}
