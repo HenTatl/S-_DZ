b56b23a759a2493ac4565a768e72f506494aef37
HenTatl <alien2amik@gmail.com>
2022-10-23 21:10:18 +0200

    Second digit

---
+++ Sem2_DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: 
+// Напишите программу, 
+// которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+
+Console.Write(" Enter natural 3-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+int SecondDig(int num)
+{
+    int nov = num / 10 % 10;
+    return nov;
+}
+int result = SecondDig(number);
+
+if (number > 100 && number < 1000) // трёхзначное число ?
+    Console.WriteLine($"{number} -> second digit -> {result}");
+else
+    Console.WriteLine("Incorrect number");
