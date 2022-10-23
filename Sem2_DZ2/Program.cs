c5cd84759ead78decaa2ef6c77a38dafba900d94
HenTatl <alien2amik@gmail.com>
2022-10-23 22:36:14 +0200

    third digit

---
+++ Sem2_DZ2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.Write("Enter natural a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = TherdNuber(number);
+  
+    if (number > 99) Console.WriteLine($"{number} -> {result}");
+
+    else if (number >= 1 && number < 100) Console.WriteLine($"{number} -> There is no third digit");
+
+    else if (number <= 0) Console.WriteLine($"{number} -> Enter a natural  number");
+
+int TherdNuber(int num) // metod
+{
+
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+
+    return num;
+
+}
+
