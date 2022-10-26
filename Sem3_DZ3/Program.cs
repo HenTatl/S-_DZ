66c71d1027737f4d707f4a497555d8c3fb02cb0a
HenTatl <alien2amik@gmail.com>
2022-10-27 01:00:14 +0200

    Cube table

---
+++ Sem3_DZ3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 23
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Clear(); 
+Console.WriteLine(); 
+
+Console.Write("Введите число:   "); 
+string writeN = Console.ReadLine()!;
+
+int N = Convert.ToInt32(writeN); 
+
+int[] GetCubeTable(int N) 
+{
+    int[] CubeArray = new int[N]; 
+    for (int i = 0; i < N; i++) 
+    {
+         CubeArray[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+
+    return CubeArray;
+}
+
+void PrintTable(int[] Array) 
+{
+
+    int number = 1; 
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine($"{number} ^ 3 = {Array[i]}");
+        number++;
+    }
+}
+
+int[] CubeTable = GetCubeTable(N);
+PrintTable(CubeTable);
