01891062a0c5acb20e6eab4407cbc1b139cebd84
HenTatl <alien2amik@gmail.com>
2022-11-14 12:50:50 +0200

    vtoraja versija

--- Sem4_DZ1/Program.cs
+++ Sem4_DZ1/Program.cs
@@ -3,3 +3,20 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int numberToPower = NumberToPower(numA, numB);
+Console.WriteLine($"Число А в степени В равно : {numberToPower}");
+
+  int NumberToPower(int numA, int numB)
+  {
+  int result = 1;
+  for(int i=1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+    return result;
+}
