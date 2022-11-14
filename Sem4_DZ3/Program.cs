3d9fb2df2da06a7be92b968af98f69e42ac941e3
HenTatl <alien2amik@gmail.com>
2022-11-14 13:17:52 +0200

    Solved

---
+++ Sem4_DZ3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел 
+// с клавиатуры и далее выводит массив на экран в одну строку.
+
+Console.Write("Введите количество элементов массива: ");
+int arrayCount = int.Parse(Console.ReadLine());
+
+int[] Array = new int[arrayCount];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}: ");
+    Array[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Вывод массива: ");
+
+for (int i = 0; i < Array.Length - (Array.Length - 1); i++)
+{
+    Console.Write($"[{String.Join(",", Array)}]");
+}
