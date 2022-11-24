557b06a0fc4c7269651218ab2f7064740f90a7ea
HenTatl <alien2amik@gmail.com>
2022-11-24 16:59:42 +0200

    Task 52

---
+++ Sem7_DZ3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Zadacha052()
+{
+    Console.WriteLine("Введите количество строк:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[,] numbers = new int[n, m];
+    FillArray(numbers);
+    Console.WriteLine();
+    PrintArray(numbers);
+    Console.WriteLine("Среднее арифметическое каждого столбца:");
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            average = (average + numbers[i, j]);
+        }
+        average = average / n;
+        Console.Write(Math.Round(average, 1) + "; " + "\t");
+    }
+}
+
+Zadacha052();
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random ();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+     for (int i = 0; i< rows; i++)
+    {
+        for (int j = 0; j< columns; j++)
+        {
+         numbers[i,j] = random.Next(-10, 11);
+        }
+        }
+}
+
+void PrintArray(int[,] numbers)
+ {
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i =0; i< rows; i++)
+    {
+        for (int j =0; j< columns; j++)
+        {
+            Console.Write(numbers[i,j] + " \t");
+        }
+        Console.WriteLine();
+    }
+ }
---
+++ Sem7_Dz1/Program.cs
@@ -0,0 +1,37 @@
+﻿//
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.Write("Количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int [,] array = FillArray(rows, columns, -10 , 10);
+
+PrintArray(array);
+
+int [,] FillArray(int rows, int columns, int min, int max){
+    int[,] array = new int[rows,columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j< columns; j++){
+            array[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] filledArray){
+    for(int i = 0; i<filledArray.GetLength(0);i++){
+        for(int j = 0; j < filledArray.GetLength(1);j++){
+            Console.Write($"{filledArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
