5b53eea52ecd1b58667219a48b370cc6c6c6f83c
HenTatl <alien2amik@gmail.com>
2022-12-07 14:41:29 +0200

    Dz7_Task47_Final

--- Sem7_Dz1/Program.cs
+++ Sem7_Dz1/Program.cs
@@ -1,37 +1,41 @@
-﻿//
+//
 // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 // m = 3, n = 4.
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-Console.Clear();
-
-Console.Write("Количество строк в массиве: ");
-int rows = int.Parse(Console.ReadLine());
-
-Console.Write("Количество столбцов в массиве: ");
-int columns = int.Parse(Console.ReadLine());
-
-int [,] array = FillArray(rows, columns, -10 , 10);
-
-PrintArray(array);
-
-int [,] FillArray(int rows, int columns, int min, int max){
-    int[,] array = new int[rows,columns];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j< columns; j++){
-            array[i,j] = new Random().Next(min, max + 1);
-        }
-    }
-    return array;
-}
-
-void PrintArray(int [,] filledArray){
-    for(int i = 0; i<filledArray.GetLength(0);i++){
-        for(int j = 0; j < filledArray.GetLength(1);j++){
-            Console.Write($"{filledArray[i,j]} ");
+Console.Write("Введите количество строк : ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов : ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[rows,columns];
+FillArray(numbers);
+Console.WriteLine("Сгенерированный массив :");
+void PrintArray()
+{
+    for (int i =0; i< rows; i++)
+    {
+        for (int j =0; j< columns; j++)
+        {
+            Console.Write(numbers[i,j] + " \t");
         }
         Console.WriteLine();
+    }
+}
+
+PrintArray();
+
+void FillArray(double[,] numbers)
+{
+    Random random = new Random ();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i< rows; i++)
+    {
+        for (int j = 0; j< columns; j++)
+        {
+         numbers[i,j] = Convert.ToDouble(random.Next(-100, 101)/10.0); 
+        }
     }
 }
